af7a19c760b691b942efe1f11f7530e28757578e
Yoshi <joseangelyoshi@gmail.com>
2020-05-01 16:27:59 +0200

    Added menu basic files && nickname ask

--- Program.cs
+++ Program.cs
@@ -9,7 +9,17 @@ namespace RPGBlowa
     {
         static void Main(string[] args)
         {
+            // Loading enemies.json file and deserializing into an Enemy list
             List<Enemy> enemies = JsonConvert.DeserializeObject<List<Enemy>>(File.ReadAllText("../../../enemies.json"));
+
+
+            // Setting up the player
+            Console.WriteLine("What is your name?");
+            string playerName = Console.ReadLine();
+
+            Player player = new Player(playerName);
+
+
         }
     }
 }
