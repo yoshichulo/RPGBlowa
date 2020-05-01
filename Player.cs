d00edaa5400aa33e4ab87cd89aad65967d910723
Yoshi <joseangelyoshi@gmail.com>
2020-05-01 14:17:57 +0200

    Added basic stats

---
+++ Player.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RPGBlowa
+{
+    class Player
+    {
+        public string Name { get; set; }
+        public int Hp { get; set; }
+        public int Def { get; set; }
+        public int Atk { get; set; }
+        public int Gold { get; set; }
+        public int Level { get; set; }
+        public int Exp { get; set; }
+
+        public double HitChance { get; set; }
+        public double DodgeChance { get; set; }
+        public double CritChance { get; set; }
+        public double CritMulti { get; set; }
+
+
+        public Player(string Name)
+        {
+            this.Name = Name;
+            this.Hp = 100;
+            this.Def = 5;
+            this.Atk = 10;
+            this.Gold = 0;
+            this.HitChance = .8;
+            this.DodgeChance = .8;
+        }
+    }
+}
